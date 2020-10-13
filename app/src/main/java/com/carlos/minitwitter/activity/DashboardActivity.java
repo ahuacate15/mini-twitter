3bf2bc688cfa3e90a5c3bdbd8ca19cc31ca5b791
ahuacate15 <carlos.itca@gmail.com>
2020-10-12 23:22:12 -0600

    ocultar boton para crear tweets en favoritos y perfil

--- app/src/main/java/com/carlos/minitwitter/activity/DashboardActivity.java
+++ app/src/main/java/com/carlos/minitwitter/activity/DashboardActivity.java
@@ -1,6 +1,8 @@
 package com.carlos.minitwitter.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.annotation.NonNull;
@@ -8,6 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -19,10 +22,12 @@ import com.carlos.minitwitter.fragment.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class DashboardActivity extends AppCompatActivity implements View.OnClickListener {
+public class DashboardActivity extends AppCompatActivity implements View.OnClickListener, NavController.OnDestinationChangedListener{
 
     private FloatingActionButton bNewTweet;
     private BottomNavigationView bottomNavigationView;
+
+    private static String TAG = "DashboardActivity";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,8 +40,12 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
 
         getSupportActionBar().hide();
 
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_tweet_fav, R.id.navigation_profile).build();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration
+                .Builder(R.id.navigation_home, R.id.navigation_tweet_fav, R.id.navigation_profile)
+                .build();
+
         NavController navController = Navigation.findNavController(this, R.id.fDashboard);
+        navController.addOnDestinationChangedListener(this::onDestinationChanged);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
@@ -51,5 +60,20 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
     private void openNewTweetFragment() {
         NewTweetFragment dialog = new NewTweetFragment();
         dialog.show(getSupportFragmentManager(), "NewTweetFragment");
+    }
+
+    @Override
+    public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+        switch (destination.getId()) {
+            case R.id.navigation_home:
+                bNewTweet.show();
+                break;
+            case R.id.navigation_tweet_fav:
+                bNewTweet.hide();
+                break;
+            case R.id.navigation_profile:
+                bNewTweet.hide();;
+                break;
+        }
     }
 }
